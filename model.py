71cf11fa1390d9fcdbaba57ed644e24858f3527a
user <ggmodhunters@gmail.com>
2024-06-15 16:52:23 +0300

    version 1.3

--- model.py
+++ model.py
@@ -1,4 +1,16 @@
 import pygame
 # map_rect=pygame.rect.Rect()
-background1=pygame.image.load('images/Tiles/sand_01.png')
-background1=pygame.transform.scale(background1,[50,50])
+kva=[]
+sbackground1=pygame.image.load('images/Tiles/sand_01.png')
+sbackground2=pygame.image.load('images/Tiles/sand_02.png')
+gbackground1=pygame.image.load('images/Tiles/grass_01.png')
+gbackground2=pygame.image.load('images/Tiles/grass_02.png')
+gbackground3=pygame.image.load('images/Tiles/grass_03.png')
+gbackground4=pygame.image.load('images/Tiles/grass_04.png')
+backgrounds=[sbackground1,sbackground2,gbackground1,gbackground2,gbackground3,gbackground4]
+
+for o in backgrounds:
+    kva.append(pygame.transform.scale(o, [50, 50]))
+backgrounds.clear()
+for i in kva:
+    backgrounds.append(i)
--- paint.py
+++ paint.py
@@ -1,12 +1,26 @@
-import pygame,model
-screen=pygame.display.set_mode([800,800])
+import random,pygame, model
+
+screen = pygame.display.set_mode([800, 800])
+x = 0
+y = 0
+
+
 def risovanie():
     pygame.display.flip()
-    screen.blit(model.background1,[0,0])
+    map_generation(model.backgrounds[0], screen)
 
 
-def map_generation(image:pygame.Surface,map_size):
-    msize=image.get_size()
-    if
-    screen.blit(model.background1,[,0])
+def map_generation(image: pygame.Surface, map_size: pygame.Surface):
+    global x, y
+    msize = image.get_size()
+    mapsize = map_size.get_size()
+    # for i in model.backgrounds:
+    if x < mapsize[0]:
 
+        screen.blit(random.choice(model.backgrounds), [x, y])
+        x += msize[0]
+    elif y < mapsize[1]:
+        y += msize[1]
+        x = 0
+    # if y!=mapsize:
+    #     screen.blit(model.background1,[x,y])
