9565c93be52248e3a3d19b3b0d5a35b0024293ce
User <gmodersincidonthaveidealol@gmail.com>
2024-07-18 16:12:53 +0300

    version 1.6.6

--- paint.py
+++ paint.py
@@ -15,7 +15,8 @@ def risovanie():
     screen.blit(model.background, [0, 0])
     for i in model.regeneration.spisoc:
         if i.collidepoint(pygame.mouse.get_pos()):
-            screen.blit(model.toweronisreximus2.backgrounds,[i.x,i.y-model.toweronisreximus2.backgrounds.get_height()])
+            igrik =i.y-(model.toweronisreximus2.backgrounds.get_height()-i.height)
+            screen.blit(model.toweronisreximus2.backgrounds,[i.x,igrik])
             print(i)
     if model.perecluthatel:
         for o in model.regeneration.spisoc:
