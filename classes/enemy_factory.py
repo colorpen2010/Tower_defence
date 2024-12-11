1d0e510b6f2e6261d0a1b15169de75738c55fdd8
GmodHunter <sdsad@gmail.com>
2024-12-11 16:06:17 +0200

    version 2.4.9

--- classes/enemy_factory.py
+++ classes/enemy_factory.py
@@ -4,7 +4,7 @@ from classes.animator import Animator
 
 
 class enem_factory():
-    def __init__(self,pyt,map,flipped=False,procent=0.5,mili_sec=100,scorost=5,spisok_tochek=None,x=100,bottom=175):
+    def __init__(self,pyt,map,flipped=False,procent=1,mili_sec=100,scorost=5,spisok_tochek=None,x=100,bottom=175):
         self.enemy=Animator(os.path.dirname(pyt),mili_sec,map,procent=procent)
         self.spisok_tochek=spisok_tochek
         self.x=x
@@ -16,17 +16,31 @@ class enem_factory():
     #     return
     def control(self,events):
         self.center=self.enemy.get_center(self.x,self.bottom)
+        self.center[0]=int(self.center[0])
+        self.center[1]=int(self.center[1])
         self.enemy.control_center(events)
         if self.spisok_tochek!=[]:
             print(self.center)
-            if self.center[0]<=self.spisok_tochek[0][0]:
-                self.x+=self.scorost
-            elif self.center[1]>=self.spisok_tochek[0][1]:
-                self.bottom-=self.scorost
-            elif self.center[0]>=self.spisok_tochek[0][0]:
-                self.x-=self.scorost
-            elif self.center[1]<=self.spisok_tochek[0][1]:
-                self.bottom+=self.scorost
+            if self.center[0]<self.spisok_tochek[0][0]:
+                if self.center[0]<=self.spisok_tochek[0][0]-self.scorost:
+                    self.x+=self.scorost
+                else:
+                    self.x+=1
+            elif self.center[1]>self.spisok_tochek[0][1]:
+                if self.center[1]>=self.spisok_tochek[0][1]+self.scorost:
+                    self.bottom-=self.scorost
+                else:
+                    self.bottom-=1
+            elif self.center[0]>self.spisok_tochek[0][0]:
+                if self.center[0] >= self.spisok_tochek[0][0] + self.scorost:
+                    self.x-=self.scorost
+                else:
+                    self.x-=1
+            elif self.center[1]<self.spisok_tochek[0][1]:
+                if self.center[1]<=self.spisok_tochek[0][1]-self.scorost:
+                    self.bottom+=self.scorost
+                else:
+                    self.bottom+=1
             else:
                 del self.spisok_tochek[0]
             # self.center[0]=self.x
